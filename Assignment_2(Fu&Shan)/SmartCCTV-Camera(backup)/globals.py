202b98826096a1098790ee281a1394e9b48187c0
Shan Lee <62006560+ShanLeeRaccoon@users.noreply.github.com>
2020-08-31 19:08:59 +0700

    Make up commit
    Make up commit for the face recognition app.(It was unsuccessful)

---
+++ Assignment_2(Fu&Shan)/SmartCCTV-Camera(backup)/globals.py
@@ -0,0 +1,9 @@
+
+def init():
+    global name
+    name = "Unknown"
+
+    global result
+    result = True
+
+
---
+++ Assignment_2(Fu&Shan)/SmartCCTV-Camera/globals.py
@@ -0,0 +1,12 @@
+
+def init():
+    global name
+    name = "Unknown"
+
+    global result
+    result = True
+
+    global run_cam
+    run_cam = True
+
+
